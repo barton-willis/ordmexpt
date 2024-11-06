d44b08c3b0dbf28a8f377ec9aeb4d1b6a8b4b085
Barton Willis <willisb@unk.edu>
2024-11-06 13:34:17 -0600

    optionally run both the old and new ordmexpt
    o To help debug, this version optionally runs both the old and new ordmexpt function, and it prints a report
    
    o Append a new test to rtest_shame.mac

--- ordmexpt.lisp
+++ ordmexpt.lisp
@@ -1,40 +1,57 @@
 #|
 Error summary:
 Error(s) found:
-       rtest_gamma.mac problems:    (384 390)
-       rtest_integrate.mac problems:
+        rtest_gamma.mac problems:    (384 390)
+        rtest_integrate.mac problems:
     (176 177 178 179 360 362 372 374 441 456 525 526 527 528 529 530 534 535
      537 538)
-       rtest_trace.mac problems:    (87 88)
-  rtest_to_poly_solve.mac problems:    (166 216)
+        rtest_trace.mac problems:    (87 88)
+  C:/Users/barto/maxima-code/share/to_poly_solve/rtest_to_poly_solve.mac problems:
+    (166 216)
 Tests that were expected to fail but passed:
-       rtest1.mac problem:    (183)
-       rtestsum.mac problem:    (95)
-       rtest_limit_extra.mac problem:    (259)
-       rtest_hg.mac problem:    (87)
- rtest_romberg.mac problem:    (18)
-  to_poly_solve/rtest_to_poly_solve.mac problem:    (322)
- rtest_raddenest.mac problem:    (123)
+        rtest1.mac problem:    (183)
+        rtestsum.mac problem:    (95)
+        rtest_limit_extra.mac problem:
+    (259)
+        rtest_hg.mac problem:
+    (87)
+  rtest_romberg.mac problem:
+    (18)
+  rtest_to_poly_solve.mac problem:
+    (322)
+  rtest_raddenest.mac problem:
+    (123)
 26 tests failed out of 18,817 total tests.
 Evaluation took:
-  359.598 seconds of real time
-  341.656250 seconds of total run time (230.265625 user, 111.390625 system)
-  [ Real times consist of 11.534 seconds GC time, and 348.064 seconds non-GC time. ]
-  [ Run times consist of 11.0000 seconds GC time, and 330.657 seconds non-GC time. ]
-  95.01% CPU
-  347,839 forms interpreted
-  347,937 lambdas converted
-  717,830,316,259 processor cycles
-  121,741,885,152 bytes consed
+  348.677 seconds of real time
+  330.437500 seconds of total run time (228.500000 user, 101.937500 system)
+  [ Real times consist of 12.807 seconds GC time, and 335.870 seconds non-GC time. ]
+  [ Run times consist of 12.093 seconds GC time, and 318.345 seconds non-GC time. ]
+  94.77% CPU
+  355,271 forms interpreted
+  355,371 lambdas converted
+  696,028,150,100 processor cycles
+  123,868,934,160 bytes consed
+
+(%o0)                                done
+(%i1) report();
+Number of old/new differences:  14957
+Calls to ordmexpt = 9,290,691
+Return old value = false
+(%o1)                                done
+
  |#
 
 (defvar *ordmexpt* nil)
 (defvar *old* nil)
-(defun ordmexptx (x y)
+(defvar *debug-ordmexpt* nil)
+(defun ordmexpt (x y)
   (let ((old (ordmexpt-old x y))
         (new (ordmexpt-new x y)))
     (when (not (eq old new))
-      (push (ftake 'mlist x y old new) *ordmexpt*))
+       (when *debug-ordmexpt* 
+          (mtell "diff: x = ~M ; y = ~M ; old = ~M ; new = ~M ~%" x y old new))
+       (push (ftake 'mlist x y old new) *ordmexpt*))
     (if *old* old new)))
 
 (defun ordmexpt-old (x y)
@@ -70,7 +87,7 @@ Evaluation took:
 
 ;; Return great(x,y), where x is an mexpt expression and y is any Maxima
 ;; expression.
-(defvar *debug-ordmexpt* nil)
+
 (defvar *calls-to-ordmexpt* 0)
 (defvar *bad* nil)
 (defun ordmexpt-new (x y)
@@ -103,9 +120,14 @@ Evaluation took:
            ((and (not x-const) y-const) t)
            ;; constant x is not greater than non-constant y
            ((and (not y-const) x-const) nil)
-           ;; rules for great(%e^X, (=/= %e)^Y) = t
-           ((eq base-x '$%e) t)
-           ((eq base-y '$%e) nil)      
+           ;; rules for great(%e^X, (=/= %e)^Y) = t. Without the %cos %sin check, we
+           ;; get an error for block([domain : complex], integrate(exp(acsc(x)),x));
+           ((and (eq base-x '$%e)) 
+            (not (and (consp y) (member (caar y) '(%cos %sin) :test #'eq)))
+             t)
+           ((and (eq base-y '$%e))  
+            (not (and (consp x) (member (caar x) '(%cos %sin) :test #'eq)))
+            nil)
            ;; default: comparison between bases
            (t (great base-x base-y))))))))
 
