fe20797c528e245dd1201c9d97bfef68a14c23b9
Barton Willis <willisb@unk.edu>
2025-07-16 15:30:46 -0500

    a few updates
    a few updates

--- ordmexpt.lisp
+++ ordmexpt.lisp
@@ -51,16 +51,9 @@ Standard Maxima--eight tests fail and
 Possibly, my ordlist function is a bit faster than the standard, and plus it is called about
 17 million fewer times.
 |#
-
-;($load "constant_subexpressions.lisp")
 ($load "approx-alike.lisp")
-($load "tlimit.lisp")
-($load "limit.lisp")
-($load "sin.lisp")
-;($load "nrat4.lisp")
 
 ;; This function is no longer used.
-
 (defun my-constantp (e &optional (constants *builtin-numeric-constants*))
  "Return t iff every leaf of Maxima expression `e` is either a number, 
   a declared system constant, or in the CL list `constants`."
@@ -70,31 +63,24 @@ Possibly, my ordlist function is a bit faster than the standard, and plus it is 
           (member e constants :test #'eq))
       (every #'my-constantp (margs e))))
 
-
-;; Return great(x,y), where x is an `mexpt` expression and y is any Maxima
-;; expression. 
+;; Return great(x,y), where x is an `mexpt` expression and y is any Maxima expression. 
 (defun ordmexpt (x y)
   "Subroutine to function 'great'. Requires `x` to be a `mexpt` expression; `y` may 
   or may not be a `mexpt` expression and `y` is *not* an `mplus` or `mtimes` 
   expression."
   ;; Decompose both x & y as x = base-x^exp-x & y = base-y^exp-y. The input x is 
   ;; required to be a mexpt expression, but y need not be a mexpt expression.
-  (let ((base-x (second x)) (exp-x (third x)) (base-y) (exp-y))
-    (if (mexptp y)
-       (setq base-y (second y)
-            exp-y (third y))
-       (setq base-y y
-            exp-y 1))
+  (let ((base-x (second x)) (exp-x (third x)))
+     (multiple-value-bind (base-y exp-y)
+        (if (mexptp y)
+            (values (second y) (third y))
+            (values y 1))
     (cond 
       ;; bases are alike; compare exponents
       ((alike1 base-x base-y)
        (great exp-x exp-y))
-      ;; make %e^X > a^Y and %e^X > a when a =/= %e.
-      ;((and (eq base-x '$%e) (not (eq base-y '$%e))) t)
-      ;((and (eq base-y '$%e) (not (eq base-x '$%e))) nil)
-
       ;; default: comparison between bases
-      (t (great base-x base-y)))))
+      (t (great base-x base-y))))))
 
 ;; Arguably, this version of `ordlist` is more tidy than is the standard version.
 ;; And I have evidence that it is more efficient than the standard version.
@@ -130,77 +116,25 @@ Possibly, my ordlist function is a bit faster than the standard, and plus it is 
               b (cdr b)))
        ;; Default case: compare heads of a and b
        (t (throw 'terminate (great (car a) (car b))))))))
-
-(defun ordfna (e a)  ; A is an atom
-  "Subroutine to function 'great'. Requires `e` to be a Maxima expression and `a` 
-   to be an atom."
-  (cond
-   ((numberp a)
-    (or (not (eq (caar e) 'rat))
-        (> (cadr e) (* (caddr e) a))))
    
-   ;; I am not sure about the (constant a)?
-   ((and (constant a) (not (member (caar e) '(mplus mtimes mexpt))))
-    (not (member (caar e) '(rat bigfloat))))
-   
-   ((eq (caar e) 'mrat))  ; All MRATs succeed all atoms
-   
-   ((null (margs e)) nil)
-   
-   ((mexptp e) (ordmexpt e a))
-   
-   ((member (caar e) '(mplus mtimes))
-    (let ((u (car (last e))))
-      (cond ((eq u a) (not (ordhack e)))
-            (t (great u a)))))
-   
-   ((eq (caar e) '%del))
-   
-   ((prog2 (setq e (car (margs e)))  ; Use first arg of e
-           (and (not (atom e)) (member (caar e) '(mplus mtimes))))
-    (let ((u (car (last e))))  ; Compare using the same procedure as above
-      (cond ((eq u a) (not (ordhack e)))
-            (t (great u a)))))
-   
-   ((eq e a))
-   
-   (t (great e a))))
-   
-
-;; An attempt to clarify and simplify (at least to me) the logic of ordfna. This version of
-;; ordfna fails the rtest_great test.
-(defun ordfna-xxx (e a)  ; A is an atom
-  "Subroutine to function 'great'. Requires `e` to be a non atom and `a` to be an atom."
-  (cond
-   ((mnump e) ; either `e` is a rational number or a bigfloat
-    (if (mnump a) 
-          (eq t (mgrp e a)) ;both `e` & `a` are numbers, so compare using mgrp
-          nil)) ;`e` is a number and `a` is not, return nill (numbers before symbols)
-
-   (($subvarp e) t) ; subvarp > atom 
-
-   ((mexptp e) (ordmexpt e a))
-
-   ((mplusp e)
-      (if (null (cdr e)) (great 0 a) (great (car (last e)) a)))
-
-   ((mtimesp e)
-      (if (null (cdr e)) (great 1 a) (great (car (last e)) a)))
-
-   (t
-      (setq e (car (margs e))) 
-      (cond ((eq e a) t)
-            (t (great e a))))))
-
-#| 
-;; This fix is needed for integrate(exp(acsc(x)),x) with domain : complex. Thanks to David Scherfgen
-;; for finding and fixing this bug.
-(defun islinear (expr var1)
-  (let ((a (let ((*islinp* t))
-             (sdiff expr var1))))
-    (if (freeof var1 a)
-      (let ((b (no-err-sub-var 0 expr var1)))
-       (if (eq b t) nil
-        (cons a (maxima-substitute 0 var1 expr)))))))
- 
- |#
+(defun ordfna (e a)			; A is an atom
+  (cond ((numberp a)
+	 (or (not (eq (caar e) 'rat))
+	     (> (cadr e) (* (caddr e) a))))
+        ((and (not (member (caar e) '(mplus mtimes mexpt)))
+              (constant a))
+	 (not (member (caar e) '(rat bigfloat))))
+	((eq (caar e) 'mrat)) ;; all MRATs succeed all atoms
+	((null (margs e)) nil)
+	((eq (caar e) 'mexpt) (ordmexpt e a))
+	((member (caar e) '(mplus mtimes))
+	 (let ((u (car (last e))))
+	   (cond ((eq u a) (not (ordhack e))) (t (great u a)))))
+	((eq (caar e) '%del))
+	((prog2 (setq e (car (margs e)))	; use first arg of e
+	     (and (not (atom e)) (member (caar e) '(mplus mtimes))))
+	 (let ((u (car (last e))))		; and compare using 
+	   (cond ((eq u a) (not (ordhack e)))	; same procedure as above
+		 (t (great u a)))))
+	((eq e a))
+	(t (great e a))))
